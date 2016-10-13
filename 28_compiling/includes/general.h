7fb1c3554c56c097b66f18557797c57fad82a21a
JesusOrtegaVilchez <jesus_a_t_g@hotmail.com>
2016-10-13 13:27:36 +0200

    new commit

--- 28_compiling/includes/general.h
+++
@@ -1,6 +0,0 @@
-#ifndef __GENERAL_H__
-#define __GENERAL_H__
-
-#define DIM 3
-
-#endif
--- 29_debuggin/interfaz.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "interfaz.h"
-
-void print_usage(const char *command){
-    fprintf(stderr,"Usage: %s <error>\n",command);
-    exit(1);
-}
